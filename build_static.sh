2fb7b3aac5c23d8a66632a5c6b857508c53e7fbb
Arvid Warnecke <arvid.warnecke@otto.de>
2017-05-19 12:21:10 +0200

    Updated golang version for static build job

--- build_static.sh
+++ build_static.sh
@@ -1,3 +1,5 @@
-docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.4.2 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
+docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" \
+	-w /app golang:1.8.1 sh \
+	-c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
 
 docker build -t madhatter/go-hello-http .
